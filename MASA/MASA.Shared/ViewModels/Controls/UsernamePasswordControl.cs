59bb2f12a3eecfacbf2c6113dcc360c921fb4ff7
Victor Lai <lai.victor.vl@gmail.com>
2014-10-18 20:49:26 -0400

    Creating UsernamePasswordControl.

---
+++ MASA/MASA.Shared/ViewModels/Controls/UsernamePasswordControl.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GalaSoft.MvvmLight;
+
+namespace MASA.ViewModels.Controls
+{
+    public class UsernamePasswordControl : ViewModelBase
+    {
+    }
+}
---
+++ MASA/MASA.Shared/Views/Controls/UsernamePasswordControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace MASA.Views.Controls
+{
+    public sealed partial class UsernamePasswordControl : UserControl
+    {
+        public UsernamePasswordControl()
+        {
+            this.InitializeComponent();
+
+            this.Loaded += (sender, args) =>
+            {
+
+            };
+
+            this.Unloaded += (sender, args) =>
+            {
+
+            };
+        }
+    }
+}
